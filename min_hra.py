7ddc363ed6ac37889a115b38086b7d07ccdd7273
Koushik Shirali <kshirali06@gmail.com>
2022-09-01 10:19:30 +0530

    refactored the code

--- min_hra.py
+++ min_hra.py
@@ -5,8 +5,8 @@ def min_hra(basic,hra,rent):
     return(min(cal_1,cal_2,(hra/12)))
 
 basic = int(input("Enter your basic amount per annum: "))
-hra = int(input("Enter the HRA given by the company: "))
-rent = int(input("Enter the rent amount:  "))
+hra = int(input("Enter the HRA given by the company per annum: "))
+rent = int(input("Enter the rent amount per month:  "))
 
 hra_ex = min_hra(basic,hra,rent)
-print(hra_ex)
+print(f"The max HR excemption you will get is {hra_ex}")
